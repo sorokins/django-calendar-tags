e7517a6cfa1b54001c144cbb8cd0f68500f8779f
Asta Kovrova <ksawie@gmail.com>
2015-02-12 17:44:08 +0300

    added yahoo calendar url template tag

--- calendartags/templatetags/calendartags.py
+++ calendartags/templatetags/calendartags.py
@@ -3,7 +3,7 @@ import urllib
 from django import template
 from ..forms import iCalendarForm
 
-from ..utils import format_datetime
+from ..utils import format_datetime, format_datetime_yahoo, format_duration_yahoo
 
 
 register = template.Library()
@@ -20,6 +20,27 @@ def google_calendar(title, start_time='', end_time='', description='', address='
         'location': address.encode('utf-8'),
     })
     url = 'https://www.google.com/calendar/render?sprop=&sprop=name:&%s' % params
+
+    return url
+
+
+@register.simple_tag
+def yahoo_calendar(title, start_time='', end_time='', description='', address=''):
+
+    duration = None
+
+    if end_time:
+        duration = end_time - start_time
+
+    params = urllib.urlencode({
+        'title': title.encode('utf-8'),
+        'in_loc': address.encode('utf-8'),
+        'desc': description.encode('utf-8'),
+        'st': format_datetime_yahoo(start_time),
+        'dur': format_duration_yahoo(duration),
+    })
+
+    url = 'http://calendar.yahoo.com/?v=60&view=d&type=20&%s' % params
 
     return url
 
--- calendartags/utils.py
+++ calendartags/utils.py
@@ -1,2 +1,9 @@
 def format_datetime(dt):
     return dt.strftime('%Y%m%dT%H%M%SZ')
+
+def format_datetime_yahoo(dt):
+    return dt.strftime('%Y%m%d%H%M%S')
+
+def format_duration_yahoo(d):
+    if d:
+        return d.strftime('%H%M')
